3e537326674462061ed58f3bdaf253e390a47c2d
skiffua <oleh_mushora@ukr.net>
2025-08-03 13:39:43 +0300

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors'
 import dotenv from 'dotenv';
 import usersRoutes from './routes/users.js';
 
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: ['http://localhost:5173', 'https://simple-form-for-edu.netlify.app/'] }))
 app.use(express.json());
 
 app.use('/users', usersRoutes);
